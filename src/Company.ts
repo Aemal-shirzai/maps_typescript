780f6332230654ba1c122b83552896a402f3f772
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-02-24 20:59:50 -0500

    [IMP] Define the company class.

--- src/Company.ts
+++ src/Company.ts
@@ -1,1 +1,20 @@
-class Company {}
+import { faker } from "@faker-js/faker";
+
+export class Company {
+    name: string;
+    catchPrase: string;
+    location: {
+        lat: number,
+        lng: number
+    }
+
+    constructor() {
+        this.name = faker.company.name();
+        this.catchPrase = faker.company.catchPhrase();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { User } from "./User";
+import { Company } from "./Company";
 
 
-var aemal = new User()
-console.log(aemal);
+var aemal = new User();
+console.log(">>>>>User: ", aemal);
+
+var netlinks = new Company();
+console.log(">>>>>>> Company: ", netlinks)
